c294584c76d64436c996df116eb7484fe5a427ec
ppalomaar <152175462+ppalomaar@users.noreply.github.com>
2026-08-12 09:09:35 +0700

    Update app.js
    selected sheets

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // Ganti dengan URL publish-to-web Google Sheet CSV
-const SHEET_URL = "PASTE_GOOGLE_SHEET_CSV_URL_HERE";
+const SHEET_URL = "https://docs.google.com/spreadsheets/d/1K0BwNSe0Q8b1wC5dNCQ_k5WeC2LmdJxk7GmTp9hZv-A/edit?usp=sharing";
 
 // Contoh:
 // https://docs.google.com/spreadsheets/d/.../gviz/tq?tqx=out:csv&sheet=Sheet1
