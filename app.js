17f72438687111fab2d1764457a11eddad46c2c6
ppalomaar <152175462+ppalomaar@users.noreply.github.com>
2026-08-12 09:06:15 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+
+// Ganti dengan URL publish-to-web Google Sheet CSV
+const SHEET_URL = "PASTE_GOOGLE_SHEET_CSV_URL_HERE";
+
+// Contoh:
+// https://docs.google.com/spreadsheets/d/.../gviz/tq?tqx=out:csv&sheet=Sheet1
